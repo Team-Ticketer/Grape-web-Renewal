304849250a53449a4e7a3b4002b4a0746fa84eb5
SayBGM <bgm537@saybgm.me>
2018-10-31 13:17:59 +0900

    Window에서 나는 오류 수정

--- frontserver.js
+++ frontserver.js
@@ -1,18 +1,2 @@
-switch (process.env.ENV) {
-  case 'LOCAL':
-    console.log('starting local...')
-    require('./frontserver.local.js')
-    break
-  case 'DEV':
-    console.log('starting dev...')
-    require('./frontserver.dev.js')
-    break
-  case 'QA':
-    console.log('starting qa...')
-    require('./frontserver.qa.js')
-    break
-  case 'REAL':
-    console.log('starting real...')
-    require('./frontserver.real.js')
-    break
-}
+console.log('starting local...')
+require('./frontserver.local.js')
