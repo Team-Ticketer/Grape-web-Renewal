358ce6d495255bcfe15a95eff6e20e3c2e51c10a
SayBGM <bgm537@saybgm.me>
2018-11-12 22:18:41 +0900

    redux 구조 수정

--- src/container/Concerts/Concerts.js
+++ src/container/Concerts/Concerts.js
@@ -16,7 +16,6 @@ class Concerts extends Component {
             {src: "/images/item-1.png", artist: "IU", startDay: '2019 09 12', endDay: '2019 09 14', place: 'Tokyo-3', cheapestPrice: 15, expensivePrice: 20}, 
             {src: "/images/item-2.png", artist: "IMG 2", startDay: '2019 09 12', endDay: '2019 09 14', place: 'Tokyo-3', cheapestPrice: 15, expensivePrice: 20},{src: "/images/item-3.png", artist: "IMG 3", startDay: '2019 09 12', endDay: '2019 09 14', place: 'Tokyo-3', cheapestPrice: 15, expensivePrice: 20}
         ],
-
         title: '',
         artist: '',
         date: '',
